eeb899e6868252336ed7017f9473d6949d7a6b9c
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2018-09-28 14:46:35 +0300

    initial commit

---
+++ src/main/java/com/pariontech/petclinic/Spring5PetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.pariontech.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5PetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pariontech/petclinic/Spring5PetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pariontech.petclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5PetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
