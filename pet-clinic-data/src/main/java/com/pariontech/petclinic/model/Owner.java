57af658a0de4d991e2e2dd8084ee289217ab35dc
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2018-10-09 02:23:07 +0300

    Added contact info to owner entity.

--- pet-clinic-data/src/main/java/com/pariontech/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/pariontech/petclinic/model/Owner.java
@@ -8,10 +8,36 @@ import java.util.Set;
  */
 
 public class Owner extends Person {
-
 	private static final long serialVersionUID = -3159864440217645540L;
 
+	private String address;
+	private String city;
+	private String telephone;
 	private Set<Pet> pets;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
