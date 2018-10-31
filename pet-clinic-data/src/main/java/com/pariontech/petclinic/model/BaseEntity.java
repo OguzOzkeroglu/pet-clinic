efc20c983084411b4ff3e0046df21e4fd253dab6
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2018-10-31 18:36:03 +0200

    Updated BaseEntity with MappedSuperclass JPA annotation

--- pet-clinic-data/src/main/java/com/pariontech/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/pariontech/petclinic/model/BaseEntity.java
@@ -2,15 +2,21 @@ package com.pariontech.petclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
- * @author oguz
- *
- * Created on 2018.10.02
+ * @author oguz, created on 2018.10.02
  */
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	private static final long serialVersionUID = 1829631081947666503L;
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
